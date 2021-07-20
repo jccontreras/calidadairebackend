c6a06a98567600014b41d8d4e114ff9b6ce4207e
ragnarok <juankcontreras00@gmail.com>
2021-07-20 17:01:19 -0500

    update sign up, doc_types and user_types

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import datetime
+
 from flask import Flask, request, jsonify, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_swagger_ui import get_swaggerui_blueprint
 import arrow
+from datetime import date
+from werkzeug.security import generate_password_hash
 
 app = Flask('__flask__')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1qaw3edr5tg@192.168.1.102:9000/calidadaireDB'
@@ -23,6 +27,88 @@ swaggerui_blueprint = get_swaggerui_blueprint(
     }
 )
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+
+# User Types
+class UserTypes(db.Model):
+    __tablename__ = 'USER_TYPES'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+
+class UserTypesSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name')
+
+
+usertype_schema = UserTypesSchema()
+usertypes_schema = UserTypesSchema(many=True)
+
+
+@app.route('/calidadaire/usertypes', methods=['POST'])
+def create_usertypes():
+    name = request.json['name']
+    new_usertype = UserTypes(name)
+    db.session.add(new_usertype)
+    db.session.commit()
+
+    return usertype_schema.jsonify(new_usertype)
+
+
+@app.route('/calidadaire/usertypes', methods=['GET'])
+def get_usertypes():
+    all_usertypes = UserTypes.query.all()
+    result = usertypes_schema.dump(all_usertypes)
+    return jsonify(result)
+
+
+@app.route('/calidadaire/usertypes/<id>', methods=['GET'])
+def get_usertype(id):
+    usertype = UserTypes.query.get(id)
+    return usertype_schema.jsonify(usertype)
+
+
+# Document Types
+class DocTypes(db.Model):
+    __tablename__ = 'DOC_TYPES'
+
+    id = db.Column(db.String(3), primary_key=True)
+    name = db.Column(db.String(10), nullable=False)
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+class DocTypesSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name')
+
+
+doctype_schema = DocTypesSchema()
+doctypes_schema = DocTypesSchema(many=True)
+
+
+@app.route('/calidadaire/doctypes', methods=['POST'])
+def create_doctypes():
+    id = request.json['id']
+    name = request.json['name']
+    new_doctype = DocTypes(id, name)
+    db.session.add(new_doctype)
+    db.session.commit()
+
+    return usertype_schema.jsonify(new_doctype)
+
+
+@app.route('/calidadaire/doctypes', methods=['GET'])
+def get_doctypes():
+    all_usertypes = DocTypes.query.all()
+    result = doctypes_schema.dump(all_usertypes)
+    return jsonify(result)
 
 
 # Particles API 's
@@ -88,6 +174,76 @@ def delete_particle(id):
     db.session.commit()
 
     return particle_schema.jsonify(particle)
+
+
+# Users API 's
+
+class User(db.Model):
+    __tablename__ = 'USERS'
+
+    id = db.Column(db.String(10), primary_key=True)
+    idtype = db.Column(db.String(3), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    cel = db.Column(db.String, nullable=False)
+    type = db.Column(db.Integer, nullable=False)
+    psw = db.Column(db.String(66), nullable=False)
+    device = db.Column(db.Integer, nullable=True)
+    bdate = db.Column(db.String, nullable=False)
+    edate = db.Column(db.String, nullable=False)
+
+    def __init__(self, id, idtype, name, email, cel, type, psw, device, bdate, edate):
+        self.id = id
+        self.idtype = idtype
+        self.name = name
+        self.email = email
+        self.cel = cel
+        self.type = type
+        self.psw = generate_psw(psw)
+        self.device = device
+        self.bdate = bdate
+        self.edate = edate
+
+
+def generate_psw(psw):
+    return generate_password_hash(psw)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'idtype', 'name', 'email', 'cel', 'type', 'psw', 'device', 'bdate', 'edate')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+@app.route('/calidadaire/signup', methods=['POST'])
+def create_user():
+    today = date.today()
+    id = request.json['id']
+    idtype = request.json['idtype']
+    name = request.json['name']
+    email = request.json['email']
+    cel = request.json['cel']
+    type = request.json['type']
+    psw = request.json['psw']
+    device = request.json['device']
+    bdate = arrow.get(request.json['bdate']).format('YYYY-MM-DD')
+    edate = arrow.get(today).format('YYYY-MM-DD')
+    new_user = User(id, idtype, name, email, cel, type,
+                    psw, device, bdate, edate)
+
+    db.session.add(new_user)
+    db.session.commit()
+    return user_schema.jsonify(new_user)
+
+
+@app.route('/calidadaire/users', methods=['GET'])
+def get_users():
+    all_user = User.query.all()
+    result = users_schema.dump(all_user)
+    return jsonify(result)
 
 
 # Devices API 's
@@ -174,7 +330,7 @@ class QualityData(db.Model):
     pressure = db.Column(db.Integer, nullable=False)
     id_particle1 = db.Column(db.Integer, nullable=False)
     value_particle1 = db.Column(db.Integer, nullable=False)
-    id_particle2 = db.Column(db.Integer, nullable=False)
+    id_particle2 = db.Column(db.Integer, nullable=False, foreign_key='PARTICLES.id')
     value_particle2 = db.Column(db.Integer, nullable=False)
     temp = db.Column(db.Integer, nullable=False)
     rh = db.Column(db.Integer)
